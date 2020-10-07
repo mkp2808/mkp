ab2bd429af883783289672d34600a64c3e805889
Manthan patel <72511044+mkp2808@users.noreply.github.com>
2020-10-07 20:36:43 +0530

    Create reverse.c

---
+++ reverse.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+void main()
+{
+    int n,r,sum=0,temp,i;
+
+    while (i>0)
+    {   
+        sum=0;
+
+        printf("Enter the value (Enter '0' to exit)\n");
+        scanf("%d",&n);
+
+        if (n==0)
+        {
+            goto exit;
+        }
+        
+
+        temp=n;
+
+        while (n>0)
+        {
+            r=n%10;
+            sum=sum*10+r;
+            n=n/10;
+        }
+        n=temp;
+    printf("Reverse Number is %d.\n",sum);
+        
+
+    }    
+    exit: printf("bye bye");
+    
+}
